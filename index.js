0de526bb114e3d0b18cac9d2d6ade1b6009fa78a
shantelle wambui <shantellewambuikungu@gmail.com>
2025-06-23 08:29:24 +0300

    Complete First Class Functions Lab: driver selectors and fare multipliers

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers= function(drivers){
+ return drivers.slice(0, 2);    
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2); // slice from the end
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier; // multiply the fare
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverFunction) {
+  return driverFunction(drivers);
+}
